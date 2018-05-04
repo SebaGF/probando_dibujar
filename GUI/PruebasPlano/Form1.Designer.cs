779070df08f09e9524fe0890a5b88a5317db5f3f
Sebastian <garciaseba96.sg@gmail.com>
2018-05-04 15:15:28 -0300

    Subida de proyecto

---
+++ GUI/PruebasPlano/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace PruebasPlano
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel.Location = new System.Drawing.Point(12, 12);
+            this.panel.MaximumSize = new System.Drawing.Size(1600, 700);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(1300, 600);
+            this.panel.TabIndex = 0;
+            this.panel.Paint += new System.Windows.Forms.PaintEventHandler(this.panel_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1924, 1055);
+            this.Controls.Add(this.panel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel;
+    }
+}
+
---
+++ GUI/PruebasPlano/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace PruebasPlano
+{
+    
+    public partial class Form1 : Form
+    {
+        private const int Meter = 30;
+        private Graphics graphic;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.graphic = this.panel.CreateGraphics();
+            //this.CreateGrilla(0, 0);
+        }
+
+        private void DrawLine(Pen pen,Point x,Point y)
+        {
+            this.graphic.DrawLine(pen, x.X, x.Y, y.X, y.Y);
+        }
+
+        private void CreateGrilla(int x, int y)
+        {
+            Point X = new Point(0, 0);
+            Point Y = new Point(100, 0);
+            Pen pen = new Pen(Color.Black, 2);
+            this.DrawLine(pen, X, Y);
+            this.panel.Refresh();
+        }
+
+        private void panel_Paint(object sender, PaintEventArgs e)
+        {
+            Pen pen = new Pen(Color.Black, 1);
+            this.graphic.DrawLine(pen, 0, 0, 1200, 0);
+            this.graphic.DrawLine(pen, 1200, 0, 1200, 1200);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
